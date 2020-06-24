e5ab98dac1efbee059462942bb614f7412e6f975
EmreAyberk <emreayberk23@gmail.com>
2020-06-25 01:50:09 +0300

    powerplant delete edit create

--- PowerPlantMvcApplication/PowerPlantMvcApplication/Controllers/PowerPlantsController.cs
+++ PowerPlantMvcApplication/PowerPlantMvcApplication/Controllers/PowerPlantsController.cs
@@ -8,7 +8,7 @@ using PowerPlantMvcApplication.Data;
 namespace PowerPlantMvcApplication.Controllers
 {
     [Authorize]
-    public class PowerPlantsController: Controller
+    public class PowerPlantsController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
 
@@ -19,8 +19,42 @@ namespace PowerPlantMvcApplication.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var list =await _dbContext.PowerPlants.ToListAsync();
+            var list = await _dbContext.PowerPlants.ToListAsync();
             return View(list);
+        }
+
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+                return View(new PowerPlant());
+            var entity = await _dbContext.PowerPlants.FirstOrDefaultAsync(e => e.Id == id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(PowerPlant input)
+        {
+            var entity = await _dbContext.PowerPlants.FirstOrDefaultAsync(e => e.Id == input.Id);
+            if (entity == null)
+            {
+                entity = new PowerPlant();
+                await _dbContext.PowerPlants.AddAsync(entity);
+            }
+            entity.Name = input.Name;
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(long id)
+        {
+            var entity = await _dbContext.PowerPlants.FirstOrDefaultAsync(e => e.Id == id);
+            if (entity != null)
+            {
+                _dbContext.PowerPlants.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return Ok(id);
         }
     }
 }
